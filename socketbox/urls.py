b3abdb2778c763032c787d8d19c5708cd0179b36
justinonstine <justin_onstine@hotmail.com>
2015-05-26 12:51:22 -0700

    added a url mapping for the user profile

--- socketbox/urls.py
+++ socketbox/urls.py
@@ -3,4 +3,6 @@ from django.conf.urls import patterns, url
 from socketbox import views
 
 urlpatterns = patterns("",
-                       url(r'^$', views.index, name='index'),)
+                       url(r'^$', views.index, name='index'),
+                       url(r'^profile$', views.profile, name='profile'),
+                       )
--- socketbox/views.py
+++ socketbox/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'socketbox/index.html', {})
+
+def profile(request):
+    return index(request)
